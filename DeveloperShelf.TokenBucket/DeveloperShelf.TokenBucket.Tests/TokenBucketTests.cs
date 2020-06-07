c321bc783aa6961cab3393689243e87c37f8c6cc
David Guerin <david@developershelf.com>
2020-06-07 11:15:31 +0100

    sorting out tests and control code

--- DeveloperShelf.TokenBucket/DeveloperShelf.TokenBucket.Tests/TokenBucketTests.cs
+++ DeveloperShelf.TokenBucket/DeveloperShelf.TokenBucket.Tests/TokenBucketTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using DeveloperShelf.TokenBucket.Manager;
@@ -12,16 +11,22 @@ namespace DeveloperShelf.TokenBucket.Tests
         /// 
         /// </summary>
         /// <param name="bucketSize">Represents the number of operations that are allowed per unit time</param>
-        /// <param name="frequencyPerSecond">Represents the amount of invocations of the function per second</param>
         /// <param name="operationDurationInMilliseconds">Represents the fake operation duration</param>
         /// <param name="cancelRequestedInMilliseconds">Represents how long the test will run for</param>
-        /// <param name="expectedInvocation">Represents the expected number of invocations of the fake function</param>
+        /// <param name="expectedInvocationRangeLower">Represents the expected lower limit of invocation of the fake function</param>
+        /// <param name="expectedInvocationRangeUpper">Represents the expected upper limit of invocations of the fake function</param>
         [Theory]
-        [InlineData(10, 2, 1000, 2000, 10)]
-        [InlineData(10, 2, 100, 2000, 10)]
-        [InlineData(100, 10, 10, 2000, 100)]
-        [InlineData(100, 20, 10, 2000, 200)]
-        public void BasicExecutionTest(int bucketSize, int frequencyPerSecond, int operationDurationInMilliseconds, int cancelRequestedInMilliseconds, int expectedInvocation)
+        [InlineData(10, 1000, 2000, 1, 2)]
+        [InlineData(10, 100, 2000, 15, 20)]
+        [InlineData(10, 1, 2000, 15, 20)]
+        [InlineData(100, 1, 2000, 100, 200)]
+        [InlineData(100, 10, 2000, 100, 200)]
+        public void BasicExecutionTest(
+            int bucketSize,
+            int operationDurationInMilliseconds, 
+            int cancelRequestedInMilliseconds,
+            int expectedInvocationRangeLower,
+            int expectedInvocationRangeUpper)
         {
             var cancellationToken = new CancellationTokenSource();
 
@@ -31,8 +36,7 @@ namespace DeveloperShelf.TokenBucket.Tests
                 await Task.Delay(operationDurationInMilliseconds, CancellationToken.None);
                 return new object();
             }, 
-            bucketSize, 
-            TimeSpan.FromSeconds(frequencyPerSecond), cancellationToken.Token);
+            bucketSize, cancellationToken.Token);
 
             var task2 = Task.Run(async () =>
             {
@@ -42,7 +46,7 @@ namespace DeveloperShelf.TokenBucket.Tests
             
             Task.WaitAll(task1, task2);
 
-            Assert.True(expectedInvocation >= manager.InvocationCount);
+            Assert.InRange(manager.InvocationCount, expectedInvocationRangeLower, expectedInvocationRangeUpper );
         }
     }
 }
